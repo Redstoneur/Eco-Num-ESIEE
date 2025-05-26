4f70325d5014e612ba0a098cc19ac4b4785e6b1a
Redstoneur <alfa.aog36@gmail.com>
2025-05-26 14:28:21 +0200

    Rename FormGroup to FormGroupNumber and update props for numeric input handling

--- front-end/src/components/form/Formulaire.vue
+++ front-end/src/components/form/Formulaire.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import {defineProps, defineEmits} from "vue";
 import FormTitle from "./FormTitle.vue";
-import FormGroup from "./FormGroup.vue";
+import FormGroupNumber from "./FormGroupNumber.vue";
 import FormSubmitButton from "./FormSubmitButton.vue";
 
 defineProps<{
@@ -39,61 +39,63 @@ defineEmits([
 <template>
   <form class="form-grid" @submit.prevent="$emit('submit')">
     <FormTitle :title="title"/>
-    <FormGroup
+    <FormGroupNumber
         id="temperature_ambiante"
         name="temperature_ambiante"
         :label="temperature_ambiante_label"
         :modelValue="temperature_ambiante"
+        :min="-273.15"
         @update:modelValue="(value) => $emit('update:temperature_ambiante', value)"
-        type="number"
     />
-    <FormGroup
+    <FormGroupNumber
         id="vitesse_vent"
         name="vitesse_vent"
         :label="vitesse_vent_label"
         :modelValue="vitesse_vent"
+        :min="0"
         @update:modelValue="(value) => $emit('update:vitesse_vent', value)"
-        type="number"
     />
-    <FormGroup
+    <FormGroupNumber
         id="intensite_courant"
         name="intensite_courant"
         :label="intensite_courant_label"
         :modelValue="intensite_courant"
+        :min="0"
         @update:modelValue="(value) => $emit('update:intensite_courant', value)"
-        type="number"
     />
-    <FormGroup
+    <FormGroupNumber
         id="temperature_cable_initiale"
         name="temperature_cable_initiale"
         :label="temperature_cable_initiale_label"
         :modelValue="temperature_cable_initiale"
+        :min="-273.15"
         @update:modelValue="(value) => $emit('update:temperature_cable_initiale', value)"
-        type="number"
     />
-    <FormGroup
+    <FormGroupNumber
         id="number_of_repetition"
         name="number_of_repetition"
         :label="number_of_repetition_label"
         :modelValue="number_of_repetition"
+        :min="1"
         @update:modelValue="(value) => $emit('update:number_of_repetition', value)"
-        type="number"
     />
-    <FormGroup
+    <FormGroupNumber
         id="simulation_duration"
         name="simulation_duration"
         :label="simulation_duration_label"
         :modelValue="simulation_duration"
+        :min="1"
         @update:modelValue="(value) => $emit('update:simulation_duration', value)"
-        type="number"
     />
-    <FormGroup
+    <FormGroupNumber
+        class="latest"
         id="time_step"
         name="time_step"
         :label="time_step_label"
         :modelValue="time_step"
+        :max="1"
+        :min="0.000001"
         @update:modelValue="(value) => $emit('update:time_step', value)"
-        type="number"
     />
     <FormSubmitButton :loading="loading" :label="buttonLabel"/>
   </form>
@@ -108,6 +110,12 @@ defineEmits([
   border-radius: 8px;
   box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
   padding: 1.5rem;
+  justify-items: center;
+  align-items: center;
+}
+
+.form-grid .latest {
+  grid-column: span 3;
 }
 
 @media (max-width: 768px) {
